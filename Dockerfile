2a38bd6440435b34a2926ed8593ff1b224101e12
Alexey Fadeev <tomsklab@yandex.ru>
2021-07-15 09:00:38 +0700

    Fixes

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN dotnet build -c release -o ./build --no-restore \
 FROM  mcr.microsoft.com/dotnet/sdk:5.0.301-alpine3.13-amd64
 WORKDIR /usr/${MODULE_NAME}/
 COPY --from=installer /usr/${MODULE_NAME}/publish .
-ENTRYPOINT dotnet ${DLL_NAME}
+ENTRYPOINT dotnet ${MODULE_NAME}.dll
---
+++ db/node/dump/modify-data.sql
@@ -0,0 +1,18 @@
+﻿WITH s1 AS MATERIALIZED (
+SELECT passenger_name, (array_agg(passenger_id))[1] AS passenger_id FROM tickets
+  GROUP BY passenger_name)
+UPDATE tickets t
+  SET passenger_id = s1.passenger_id
+  FROM s1
+  WHERE t.passenger_name=s1.passenger_name;
+
+CREATE VIEW public.passengers AS
+  SELECT passenger_id AS id, passenger_name AS name
+  FROM tickets;
+
+INSERT INTO graphql.additional_foreign_keys(
+            column_name, foreign_table_name, foreign_column_name, table_name)
+    VALUES ('passenger_id', 'passengers', 'id', 'flights');
+
+REFRESH MATERIALIZED VIEW graphql.schema_foreign_keys;
+REFRESH MATERIALIZED VIEW graphql.schema_columns;
