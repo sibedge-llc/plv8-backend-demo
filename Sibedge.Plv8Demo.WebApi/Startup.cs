c8bdc1b679e0bd70efeeb29bf36b0e2330c6c25c
Alexey Fadeev <fadeevas@sibedge.com>
2023-11-29 12:22:44 +0700

    PLV8 Framework update

--- Sibedge.Plv8Demo.WebApi/Startup.cs
+++ Sibedge.Plv8Demo.WebApi/Startup.cs
@@ -4,7 +4,6 @@
     using Filters;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
